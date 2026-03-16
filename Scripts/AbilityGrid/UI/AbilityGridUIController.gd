c98500464b4dcbd6d7b7a14e08ae1c4be631f4a3
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2026-03-16 22:07:36 +0800

    Refactored AbiltiyGridUIController.

--- Scripts/AbilityGrid/UI/AbilityGridUIController.gd
+++ Scripts/AbilityGrid/UI/AbilityGridUIController.gd
@@ -1,162 +1,199 @@
-class_name AbilityGridUIController extends Control
+class_name AbilityGridUIController extends BaseAbilityGridUIController
 
 @export var ability_grid_ui : AbilityGridUI
 @export var ability_inventory_ui : AbilityGridUI
 
-@onready var cursor_ui: CursorUI = %CursorUI
- 
-var player : PlayerController
-
-var _hovered_ui : AbilityGridUI = null
-var _held_tile : AbilityTile = null
-var _source_grid : AbilityGrid = null
-var _original_grid : AbilityGrid
-var _original_pos : Vector2i
-var _original_rotation_idx : int = 0
-
-signal clicked_void()
-
 func _ready() -> void:
+	visible = false
 	player = PlayerServer.main_player
 	
 	player.initialized_grids.connect(
 		func(ability_grid : AbilityGrid, ability_inventory : AbilityGrid):
 			ability_grid_ui.generate_grid_ui(ability_grid)
 			ability_inventory_ui.generate_grid_ui(ability_inventory)
-			pass
 	)
-	#player.ready.connect(
-		#func():
-			#ability_grid_ui.generate_grid_ui(player.ability_grid)
-	#)
-	#player.ready.connect(
-		#func():
-			#ability_inventory_ui.generate_grid_ui(player.ability_tile_inventory)
-			#pass
-	#)
-	
-	visible = false
+	pass
 
 func open_ui():
 	visible = !visible
 	
 	if visible:
-		
-		ability_grid_ui.mouse_entered.connect(_on_ability_grid_ui_mouse_entered)
-		ability_grid_ui.slot_clicked.connect(_on_ability_grid_ui_slot_clicked)
-		
-		ability_inventory_ui.mouse_entered.connect(_on_ability_inventory_ui_mouse_entered)
-		ability_inventory_ui.slot_clicked.connect(_on_ability_grid_ui_slot_clicked)
+		setup_grid_signals(ability_grid_ui)
+		setup_grid_signals(ability_inventory_ui)
+		pass
 	else:
 		cursor_ui.clear()
-		
-		ability_grid_ui.mouse_entered.disconnect(_on_ability_grid_ui_mouse_entered)
-		ability_grid_ui.slot_clicked.disconnect(_on_ability_grid_ui_slot_clicked)
-		
-		ability_inventory_ui.mouse_entered.disconnect(_on_ability_inventory_ui_mouse_entered)
-		ability_inventory_ui.slot_clicked.disconnect(_on_ability_grid_ui_slot_clicked)
-	pass
-
-
+		disconnect_grid_signals(ability_grid_ui)
+		disconnect_grid_signals(ability_inventory_ui)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if event.keycode == KEY_TAB and event.pressed:
-			if !visible:
-				print("OPEN ABILITY GRID UI")
-			if visible:
-				print("CLOSE ABILITY GRID UI")
-				if _held_tile != null:
-					pass
-			open_ui()
-	
-	if event is InputEventKey and _held_tile != null:
-		if event.keycode == KEY_R and event.pressed:
-			#WIP : TILE ROTATION
-			pass
+	if event is InputEventKey and event.keycode == KEY_TAB and event.pressed:
+		open_ui()
 		pass
 	
-	if event is InputEventKey:
-		if event.keycode == KEY_I and event.pressed:
-			ability_grid_ui.show_locked_slots()
-			ability_grid_ui.get_parent().update_minimum_size()
-			pass 
-	pass
-
-func _process(delta: float) -> void:
-	if visible:
-		cursor_ui.follow_mouse_pos(get_local_mouse_position())
-
-func _gui_input(event: InputEvent) -> void:
-	
-	pass
-
-func _on_ability_grid_ui_slot_clicked(slot_pos : Vector2i, ability_grid : AbilityGrid):
-	#Pickup
-	if _held_tile == null:
-		var tile : AbilityTile = ability_grid.get_tile_on_slot(slot_pos)
-		if tile != null:
-			var tile_rect : AbilityTileTextureRect = _hovered_ui.pop_tile_rect(tile)
-			tile_rect.position = Vector2.ZERO - tile_rect.get_root_offset_position(tile_rect.ability_tile.offsets)#tile_rect.calculate_pos_relative_to_grid(tile, Vector2i(0,0))
-			cursor_ui.add_child(tile_rect)
-			
-			var adjacent_tiles : Dictionary[Vector2i, AbilityTile] = ability_grid.get_adjacent_tiles(tile)
-			
-			_held_tile = tile
-			_original_grid = ability_grid
-			_original_pos = ability_grid.ability_tiles.get(tile)
-			_original_rotation_idx = tile.rotation_index
-			ability_grid.remove_tile_on_slot(slot_pos)
-			
-			_update_adjacent_tiles(adjacent_tiles, ability_grid)
-			tile.update_adjacent_tiles({})
-			return
-	#Place
-	if _held_tile != null:
-		if ability_grid.place_tile_on_slot(_held_tile, slot_pos):
-			#_held_tile.adjacent_tiles = ability_grid.get_adjacent_tiles_from_adjacent_points(_held_tile)
-			
-			_held_tile.update_adjacent_tiles(ability_grid.get_adjacent_tiles_from_adjacent_points(_held_tile))
-			var adjacent_tiles : Dictionary[Vector2i, AbilityTile] = ability_grid.get_adjacent_tiles(_held_tile)
-			_update_adjacent_tiles(adjacent_tiles, ability_grid)
-			
-			_held_tile = null
-			_original_grid = null
-			_original_pos = Vector2i(-1,-1)
-			cursor_ui.clear()
-			return
-		#Rollback Placement
-		else:
-			_held_tile.set_rotation_to(_original_rotation_idx)
-			_original_grid.place_tile_on_slot(_held_tile, _original_pos)
-			
-			#_held_tile.adjacent_tiles = ability_grid.get_adjacent_tiles_from_adjacent_points(_held_tile)
-			_held_tile.update_adjacent_tiles(_original_grid.get_adjacent_tiles_from_adjacent_points(_held_tile))
-			var adjacent_tiles : Dictionary[Vector2i, AbilityTile] = _original_grid.get_adjacent_tiles(_held_tile)
-			_update_adjacent_tiles(adjacent_tiles, _original_grid)
-			
-			_held_tile = null
-			_original_grid = null
-			_original_pos = Vector2i(-1,-1)
-			cursor_ui.clear()
-			return
-	pass
-
-func _update_adjacent_tiles(tiles : Dictionary[Vector2i, AbilityTile], grid : AbilityGrid):
-	for tile in tiles:
-		var ability_tile : AbilityTile = tiles[tile]
-		var adjacent_tiles : Dictionary[Vector2i, AbilityTile] = grid.get_adjacent_tiles_from_adjacent_points(ability_tile)
-		print("UPDATE ADJACENT TILE: " + ability_tile.name + " ADJACENT TILES: " + str(adjacent_tiles))
-		ability_tile.update_adjacent_tiles(adjacent_tiles)
+	if event is InputEventKey and event.keycode == KEY_I and event.pressed:
+		ability_grid_ui.show_locked_slots()
+		ability_grid_ui.get_parent().update_minimum_size()
 		pass
-	pass
+	
 
-func _on_ability_grid_ui_mouse_entered():
-	print("HOVERED ABILITY GRID UI")
-	_hovered_ui = ability_grid_ui
-	pass
-
-func _on_ability_inventory_ui_mouse_entered():
-	print("HOVERED ABILITY INVENTORY GRID UI")
-	_hovered_ui = ability_inventory_ui
-	pass
+#@export var ability_grid_ui : AbilityGridUI
+#@export var ability_inventory_ui : AbilityGridUI
+#
+#@onready var cursor_ui: CursorUI = %CursorUI
+ #
+#var player : PlayerController
+#
+#var _hovered_ui : AbilityGridUI = null
+#var _held_tile : AbilityTile = null
+#var _source_grid : AbilityGrid = null
+#var _original_grid : AbilityGrid
+#var _original_pos : Vector2i
+#var _original_rotation_idx : int = 0
+#
+#signal clicked_void()
+#
+#func _ready() -> void:
+	#player = PlayerServer.main_player
+	#
+	#player.initialized_grids.connect(
+		#func(ability_grid : AbilityGrid, ability_inventory : AbilityGrid):
+			#ability_grid_ui.generate_grid_ui(ability_grid)
+			#ability_inventory_ui.generate_grid_ui(ability_inventory)
+			#pass
+	#)
+	##player.ready.connect(
+		##func():
+			##ability_grid_ui.generate_grid_ui(player.ability_grid)
+	##)
+	##player.ready.connect(
+		##func():
+			##ability_inventory_ui.generate_grid_ui(player.ability_tile_inventory)
+			##pass
+	##)
+	#
+	#visible = false
+#
+#func open_ui():
+	#visible = !visible
+	#
+	#if visible:
+		#
+		#ability_grid_ui.mouse_entered.connect(_on_ability_grid_ui_mouse_entered)
+		#ability_grid_ui.slot_clicked.connect(_on_ability_grid_ui_slot_clicked)
+		#
+		#ability_inventory_ui.mouse_entered.connect(_on_ability_inventory_ui_mouse_entered)
+		#ability_inventory_ui.slot_clicked.connect(_on_ability_grid_ui_slot_clicked)
+	#else:
+		#cursor_ui.clear()
+		#
+		#ability_grid_ui.mouse_entered.disconnect(_on_ability_grid_ui_mouse_entered)
+		#ability_grid_ui.slot_clicked.disconnect(_on_ability_grid_ui_slot_clicked)
+		#
+		#ability_inventory_ui.mouse_entered.disconnect(_on_ability_inventory_ui_mouse_entered)
+		#ability_inventory_ui.slot_clicked.disconnect(_on_ability_grid_ui_slot_clicked)
+	#pass
+#
+#
+#
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event is InputEventKey:
+		#if event.keycode == KEY_TAB and event.pressed:
+			#if !visible:
+				#print("OPEN ABILITY GRID UI")
+			#if visible:
+				#print("CLOSE ABILITY GRID UI")
+				#if _held_tile != null:
+					#pass
+			#open_ui()
+	#
+	#if event is InputEventKey and _held_tile != null:
+		#if event.keycode == KEY_R and event.pressed:
+			##WIP : TILE ROTATION
+			#pass
+		#pass
+	#
+	#if event is InputEventKey:
+		#if event.keycode == KEY_I and event.pressed:
+			#ability_grid_ui.show_locked_slots()
+			#ability_grid_ui.get_parent().update_minimum_size()
+			#pass 
+	#pass
+#
+#func _process(delta: float) -> void:
+	#if visible:
+		#cursor_ui.follow_mouse_pos(get_local_mouse_position())
+#
+#func _gui_input(event: InputEvent) -> void:
+	#
+	#pass
+#
+#func _on_ability_grid_ui_slot_clicked(slot_pos : Vector2i, ability_grid : AbilityGrid):
+	##Pickup
+	#if _held_tile == null:
+		#var tile : AbilityTile = ability_grid.get_tile_on_slot(slot_pos)
+		#if tile != null:
+			#var tile_rect : AbilityTileTextureRect = _hovered_ui.pop_tile_rect(tile)
+			#tile_rect.position = Vector2.ZERO - tile_rect.get_root_offset_position(tile_rect.ability_tile.offsets)#tile_rect.calculate_pos_relative_to_grid(tile, Vector2i(0,0))
+			#cursor_ui.add_child(tile_rect)
+			#
+			#var adjacent_tiles : Dictionary[Vector2i, AbilityTile] = ability_grid.get_adjacent_tiles(tile)
+			#
+			#_held_tile = tile
+			#_original_grid = ability_grid
+			#_original_pos = ability_grid.ability_tiles.get(tile)
+			#_original_rotation_idx = tile.rotation_index
+			#ability_grid.remove_tile_on_slot(slot_pos)
+			#
+			#_update_adjacent_tiles(adjacent_tiles, ability_grid)
+			#tile.update_adjacent_tiles({})
+			#return
+	##Place
+	#if _held_tile != null:
+		#if ability_grid.place_tile_on_slot(_held_tile, slot_pos):
+			##_held_tile.adjacent_tiles = ability_grid.get_adjacent_tiles_from_adjacent_points(_held_tile)
+			#
+			#_held_tile.update_adjacent_tiles(ability_grid.get_adjacent_tiles_from_adjacent_points(_held_tile))
+			#var adjacent_tiles : Dictionary[Vector2i, AbilityTile] = ability_grid.get_adjacent_tiles(_held_tile)
+			#_update_adjacent_tiles(adjacent_tiles, ability_grid)
+			#
+			#_held_tile = null
+			#_original_grid = null
+			#_original_pos = Vector2i(-1,-1)
+			#cursor_ui.clear()
+			#return
+		##Rollback Placement
+		#else:
+			#_held_tile.set_rotation_to(_original_rotation_idx)
+			#_original_grid.place_tile_on_slot(_held_tile, _original_pos)
+			#
+			##_held_tile.adjacent_tiles = ability_grid.get_adjacent_tiles_from_adjacent_points(_held_tile)
+			#_held_tile.update_adjacent_tiles(_original_grid.get_adjacent_tiles_from_adjacent_points(_held_tile))
+			#var adjacent_tiles : Dictionary[Vector2i, AbilityTile] = _original_grid.get_adjacent_tiles(_held_tile)
+			#_update_adjacent_tiles(adjacent_tiles, _original_grid)
+			#
+			#_held_tile = null
+			#_original_grid = null
+			#_original_pos = Vector2i(-1,-1)
+			#cursor_ui.clear()
+			#return
+	#pass
+#
+#func _update_adjacent_tiles(tiles : Dictionary[Vector2i, AbilityTile], grid : AbilityGrid):
+	#for tile in tiles:
+		#var ability_tile : AbilityTile = tiles[tile]
+		#var adjacent_tiles : Dictionary[Vector2i, AbilityTile] = grid.get_adjacent_tiles_from_adjacent_points(ability_tile)
+		#print("UPDATE ADJACENT TILE: " + ability_tile.name + " ADJACENT TILES: " + str(adjacent_tiles))
+		#ability_tile.update_adjacent_tiles(adjacent_tiles)
+		#pass
+	#pass
+#
+#func _on_ability_grid_ui_mouse_entered():
+	#print("HOVERED ABILITY GRID UI")
+	#_hovered_ui = ability_grid_ui
+	#pass
+#
+#func _on_ability_inventory_ui_mouse_entered():
+	#print("HOVERED ABILITY INVENTORY GRID UI")
+	#_hovered_ui = ability_inventory_ui
+	#pass
---
+++ Scripts/AbilityGrid/UI/BaseAbilityGridUIController.gd
@@ -0,0 +1,96 @@
+class_name BaseAbilityGridUIController extends Control
+
+
+@onready var cursor_ui: CursorUI = %CursorUI
+
+var player : PlayerController
+var _hovered_ui : AbilityGridUI = null
+var _held_tile : AbilityTile = null
+var _original_grid : AbilityGrid
+var _original_pos : Vector2i
+var _original_rotation_idx : int = 0
+
+
+func _process(delta: float) -> void:
+	if visible:
+		cursor_ui.follow_mouse_pos(get_local_mouse_position())
+
+func setup_grid_signals(grid_ui : AbilityGridUI):
+	grid_ui.mouse_entered.connect(_on_grid_mouse_entered.bind(grid_ui))
+	grid_ui.slot_clicked.connect(_on_slot_clicked)
+	pass
+
+func disconnect_grid_signals(grid_ui : AbilityGridUI):
+	grid_ui.mouse_entered.disconnect(_on_grid_mouse_entered.bind(grid_ui))
+	grid_ui.slot_clicked.disconnect(_on_slot_clicked)
+	pass
+
+func _on_slot_clicked(slot_pos : Vector2i, grid : AbilityGrid):
+	if _held_tile == null:
+		_pick_up_tile(slot_pos, grid)
+		pass
+	else:
+		_attempt_placement(slot_pos, grid)
+	pass
+
+func _pick_up_tile(slot_pos : Vector2i, grid : AbilityGrid):
+	var tile : AbilityTile = grid.get_tile_on_slot(slot_pos)
+	if tile == null : return
+	
+	var tile_rect : AbilityTileTextureRect = _hovered_ui.pop_tile_rect(tile)
+	tile_rect.position = Vector2.ZERO - tile_rect.get_root_offset_position(tile.offsets)
+	cursor_ui.add_child(tile_rect)
+	
+	var adjacent_tiles : Dictionary[Vector2i, AbilityTile] = grid.get_adjacent_tiles(tile)
+	_held_tile = tile
+	_original_grid = grid
+	_original_pos = grid.ability_tiles.get(tile)
+	_original_rotation_idx = tile.rotation_index
+	
+	grid.remove_tile_on_slot(slot_pos)
+	_update_adjacent_tiles(adjacent_tiles, grid)
+	tile.update_adjacent_tiles({})
+	pass
+
+func _attempt_placement(slot_pos : Vector2i, grid : AbilityGrid):
+	if can_place_tile(_held_tile, grid, slot_pos):
+		_confirm_placement(slot_pos, grid)
+		pass
+	else:
+		_rollback_placement()
+	pass
+
+func can_place_tile(tile : AbilityTile, grid : AbilityGrid, pos : Vector2i) -> bool:
+	return grid.can_place(tile, pos)
+
+func _confirm_placement(slot_pos : Vector2i, grid : AbilityGrid):
+	if grid.place_tile_on_slot(_held_tile, slot_pos):
+		_held_tile.update_adjacent_tiles(grid.get_adjacent_tiles_from_adjacent_points(_held_tile))
+		_update_adjacent_tiles(grid.get_adjacent_tiles(_held_tile), grid)
+		_clear_helt_state()
+		pass
+	pass
+
+func _rollback_placement():
+	_held_tile.set_rotation_to(_original_rotation_idx)
+	_original_grid.place_tile_on_slot(_held_tile, _original_pos)
+	_held_tile.update_adjacent_tiles(_original_grid.get_adjacent_tiles_from_adjacent_points(_held_tile))
+	_update_adjacent_tiles(_original_grid.get_adjacent_tiles(_held_tile), _original_grid)
+	pass
+
+func _clear_helt_state():
+	_held_tile = null
+	_original_grid = null
+	_original_pos = Vector2i(-1, -1)
+	cursor_ui.clear()
+	pass
+
+func _update_adjacent_tiles(tiles : Dictionary[Vector2i, AbilityTile], grid : AbilityGrid):
+	for tile_coord in tiles:
+		var a_tile : AbilityTile = tiles[tile_coord]
+		a_tile.update_adjacent_tiles(grid.get_adjacent_tiles_from_adjacent_points(a_tile))
+	pass
+
+func _on_grid_mouse_entered(grid_ui : AbilityGridUI):
+	_hovered_ui = grid_ui
+	pass
