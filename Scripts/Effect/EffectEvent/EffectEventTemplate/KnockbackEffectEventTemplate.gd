c4a7b15e2b9b86e4648241808c9d8f5f98527081
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2026-03-17 17:45:42 +0800

    Minor edit to Knockback System.

--- Scripts/Effect/EffectEvent/EffectEventTemplate/KnockbackEffectEventTemplate.gd
+++ Scripts/Effect/EffectEvent/EffectEventTemplate/KnockbackEffectEventTemplate.gd
@@ -1,8 +1,17 @@
 class_name KnockbackEffectEventTemplate extends EffectEventTemplate
 
-@export var magnitude : float = 10
+@export var magnitude : ValueProviderTemplate
+
+var magnitude_value_provider : ValueProvider
 
 func build_effect_event(effect : Effect) -> EffectEvent:
+	if magnitude == null:
+		magnitude = FlatValueProviderTemplate.new()
+		magnitude.value = 10
+		magnitude_value_provider = magnitude.build_value_provider(effect.effect_context)
+	else:
+		magnitude_value_provider = magnitude.build_value_provider(effect.effect_context)
+	
 	var knockback_event : KnockbackEffectEvent = KnockbackEffectEvent.new()
-	knockback_event.magnitude = magnitude
+	knockback_event.magnitude = magnitude_value_provider.get_value(effect.effect_context)
 	return knockback_event
