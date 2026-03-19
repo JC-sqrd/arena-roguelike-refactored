b3b02858fceabee7833fbf3b201041d6bbd58af1
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2026-03-19 15:57:59 +0800

    Implemented dual weapon wield.

--- Scripts/Equipment/EquipmentManager/EquipmentManager.gd
+++ Scripts/Equipment/EquipmentManager/EquipmentManager.gd
@@ -7,10 +7,18 @@ var equipped_weapon : Weapon
 
 @export_category("Main Weapon Slot")
 @export var main_weapon_slot : EquipSlot
+@export_category("Second Weapon Slot")
+@export var second_weapon_slot : EquipSlot
 @export var attack_input_listener : AttackInputListener
 
 var wielder : Entity
 var stats : Stats
+
+var weapon_1_controller : WeaponController
+var weapon_2_controller : WeaponController
+
+var weapon_1_input_held : bool = false
+var weapon_2_input_held : bool = false
 
 signal weapon_equipped(weapon : Weapon)
 signal weapon_unequipped(weapon : Weapon)
@@ -20,8 +28,14 @@ func initialize(wielder : Entity):
 	self.wielder = wielder
 	
 	attack_input_listener.initialize(stats)
+	if main_weapon_slot != null and main_weapon_slot.equipped_equipment != null:
+		equip_weapon(main_weapon_slot.equipped_equipment)
+		weapon_1_controller = (main_weapon_slot.equipped_equipment as Weapon).get_active_controller()
 	
-	equip_weapon(main_weapon_slot.equipped_equipment)
+	if second_weapon_slot != null and second_weapon_slot.equipped_equipment != null:
+		equip_weapon(second_weapon_slot.equipped_equipment)
+		weapon_2_controller = (second_weapon_slot.equipped_equipment as Weapon).get_active_controller()
+		pass
 	pass
 
 func equip_weapon(equipment : Weapon):
@@ -40,4 +54,49 @@ func unequip_weapon():
 	var weapon : Weapon = main_weapon_slot.unequip()
 	equipped_weapon = null
 	weapon_unequipped.emit(weapon)
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_pressed("attack"):
+		print("WEAPON 1 ATTACK")
+		weapon_1_input_held = true
+		pass
+	
+	if Input.is_action_pressed("attack2"):
+		print("WEAPON 2 ATTACK")
+		weapon_2_input_held = true
+		pass
+	
+	if Input.is_action_just_released("attack"):
+		weapon_1_input_held = false
+		pass
+	
+	if Input.is_action_just_released("attack2"):
+		weapon_2_input_held = false
+		pass
+	
+	if weapon_1_input_held and !weapon_2_input_held:
+		if weapon_1_controller != null:
+			weapon_1_controller.start_attack()
+		pass
+	elif weapon_2_input_held and !weapon_1_input_held:
+		if weapon_2_controller != null:
+			weapon_2_controller.start_attack()
+		pass
+	elif weapon_1_input_held and weapon_2_input_held:
+		if weapon_1_controller != null:
+			weapon_1_controller.start_attack()
+		pass
+	#if _input_held:
+	
+	
+	pass
+
+func on_weapon_unequipped(weapon : Weapon):
+	if weapon == main_weapon_slot.equipped_equipment:
+		weapon_1_controller = null
+		pass
+	elif weapon == second_weapon_slot.equipped_equipment:
+		weapon_2_controller = null
+		pass
 	pass
