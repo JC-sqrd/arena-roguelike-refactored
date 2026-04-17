10ce66c16d75c30d48d075cde43acd15a97f7810
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2026-04-17 21:01:20 +0800

    Implemented queue system to EffectServer for receiving effects.

--- Scripts/Debug/debug_gold_ui.gd
+++ Scripts/Debug/debug_gold_ui.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var gold_label: Label = %GoldLabel
+@onready var active_entities_label: Label = %ActiveEntitiesLabel
 
 
 func _ready() -> void:
@@ -8,6 +9,9 @@ func _ready() -> void:
 	CurrencyServer.gold_subtracted.connect(_on_gold_subtracted)
 	gold_label.text = "GOLD: " + str(CurrencyServer.current_gold)
 	pass
+
+func _process(delta: float) -> void:
+	active_entities_label.text = str(EntityServer.active_entities.size())
 
 func _on_gold_added(added : float):
 	gold_label.text = "GOLD: " + str(CurrencyServer.current_gold)
--- Scripts/Servers/EffectServer.gd
+++ Scripts/Servers/EffectServer.gd
@@ -2,7 +2,8 @@
 extends Node
 
 var effect_listeners : Dictionary[RID, EffectListener]
-
+var effect_queue : Array[Dictionary]
+var service_arr : Array[Dictionary]
 
 func register_effect_listener(rid : RID, effect_listener : EffectListener):
 	effect_listeners[rid] = effect_listener
@@ -12,12 +13,37 @@ func _process(delta: float) -> void:
 	for listener in effect_listeners:
 		effect_listeners[listener].update_durational_effects(delta)
 		pass
-
+	
+	if effect_queue.is_empty():
+		return
+	
+	for i in range(6):
+		if i >= effect_queue.size():
+			return
+		var rid : RID = effect_queue[i].rid
+		var effect_listener : EffectListener = effect_listeners.get(rid)
+		var effect : Effect = effect_queue[i].effect
+		var context : Dictionary[StringName, Variant] = effect_queue[i].context
+		print("EFFECT CONTEXT: ",str(context))
+		if effect_listener != null:
+			effect.effect_context = context
+			effect.effect_context.target_rid = rid
+			effect_listener.receive_effect(effect)
+		effect_queue.remove_at(i)
+		pass
 func free_rid(rid : RID):
 	effect_listeners.erase(rid)
 	pass
 
 func receive_effect(rid : RID, effect : Effect, context : Dictionary[StringName,Variant]):
+	var data : Dictionary[StringName, Variant]
+	data["rid"] = rid
+	data["effect"] = effect
+	data["context"] = context
+	
+	effect_queue.append(data)
+	return
+	
 	var effect_listener : EffectListener = effect_listeners.get(rid)
 	print("EFFECT CONTEXT: ",str(context))
 	if effect_listener != null:
