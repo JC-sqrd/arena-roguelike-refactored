aff4805ac62c6c3af3228fb0f00e50664580f26c
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2026-04-21 22:51:15 +0800

    Addendum of Particle emission of CursorUI upon upgrading an AbilityTile.

--- Scripts/AbilityGrid/UI/AbilityGridCursorUI.gd
+++ Scripts/AbilityGrid/UI/AbilityGridCursorUI.gd
@@ -1,8 +1,14 @@
 class_name AbilityGridCursorUI extends CursorUI
 
-@onready var upgrade_particles: GPUParticles2D = $UpgradeParticles
+@onready var spark_particles: GPUParticles2D = %SparkParticles
+@onready var circle_particle: GPUParticles2D = %CircleParticle
+@onready var spark_particles_2: GPUParticles2D = %SparkParticles2
 
 func emit_upgrade_particles():
-	upgrade_particles.restart()
-	upgrade_particles.emitting = true
+	spark_particles.restart()
+	circle_particle.restart()
+	spark_particles_2.restart()
+	circle_particle.emitting = true
+	spark_particles.emitting = true
+	spark_particles_2.emitting = true
 	pass
