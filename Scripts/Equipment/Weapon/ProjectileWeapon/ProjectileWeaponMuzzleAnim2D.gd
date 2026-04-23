00a88b3e2e2aa3d8df79aa06bdb8f0b75ea8224b
JC-sqrd <j.tadena.537125@umindanao.edu.ph>
2026-04-23 16:02:09 +0800

    Updated Baseball Swing animation and Uzi bullet sprites.

--- Scripts/Equipment/Weapon/ProjectileWeapon/ProjectileWeaponMuzzleAnim2D.gd
+++ Scripts/Equipment/Weapon/ProjectileWeapon/ProjectileWeaponMuzzleAnim2D.gd
@@ -2,19 +2,28 @@ class_name ProjectileWeaponMuzzleAnim2D extends AnimatedSprite2D
 
 
 var projectile_weapon_controller : ProjectileWeaponController
+@export var timer : Timer
 
 func initialize(controller : ProjectileWeaponController):
 	#projectile_weapon_controller = controller
 	controller.attack_started.connect(_on_weapon_attack_started)
-	#controller.attack_end.connect(_on_weaon_attack_end)
-	animation_finished.connect(_on_animation_finished)
+	controller.attack_end.connect(_on_weaon_attack_end)
+	#animation_finished.connect(_on_animation_finished)
+	timer.wait_time = 0.2
+	timer.timeout.connect(_on_timeout)
 	pass
 
 func _on_weapon_attack_started():
-	stop()
-	play("muzzle")
+	if !(animation == "muzzle"):
+		sprite_frames.set_animation_loop("muzzle", true)
+		play("muzzle")
 	pass
 
-func _on_animation_finished():
+func _on_weaon_attack_end():
+	timer.start()
+	pass
+
+func _on_timeout():
+	sprite_frames.set_animation_loop("muzzle", false)
 	play("default")
 	pass
